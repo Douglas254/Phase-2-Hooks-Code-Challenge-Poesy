68b00be9ee765c91bdf3b0b39bbda301ac55a2a2
Douglas Obara <douglasobara97@gmail.com>
2022-07-23 09:21:44 +0300

    render the poems on the browser from the db

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PoemsContainer from "./PoemsContainer";
 import NewPoemForm from "./NewPoemForm";
 
 function App() {
+  const [poems, setPoems] = useState([]);
+  // fetch poems from db
+  useEffect(() => {
+    fetch(" http://localhost:8004/poems")
+      .then((res) => res.json())
+      .then((ourPoems) => setPoems(ourPoems));
+  }, []);
+
   return (
     <div className="app">
       <div className="sidebar">
         <button>Show/hide new poem form</button>
         {true ? <NewPoemForm /> : null}
       </div>
-      <PoemsContainer />
+      <PoemsContainer poems={poems} />
     </div>
   );
 }
