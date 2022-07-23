e55cbcd2a4b66088911d9818190bc867f453f9d2
Douglas Obara <douglasobara97@gmail.com>
2022-07-23 09:34:09 +0300

    use conditional rendering to show/hide the poem form

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import NewPoemForm from "./NewPoemForm";
 
 function App() {
   const [poems, setPoems] = useState([]);
+  const [myShowForm, setMyShowForm] = useState(false);
+
   // fetch poems from db
   useEffect(() => {
     fetch(" http://localhost:8004/poems")
@@ -11,11 +13,19 @@ function App() {
       .then((ourPoems) => setPoems(ourPoems));
   }, []);
 
+  // function to show/hide the button
+  function handleButtonClick() {
+    // return true if myShowForm is false otherwise true
+    setMyShowForm((myShowForm) => !myShowForm);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
-        <button>Show/hide new poem form</button>
-        {true ? <NewPoemForm /> : null}
+        <button onClick={handleButtonClick}>
+          {myShowForm ? "Hide" : "Show"} new poem form
+        </button>
+        {myShowForm ? <NewPoemForm /> : null}
       </div>
       <PoemsContainer poems={poems} />
     </div>
